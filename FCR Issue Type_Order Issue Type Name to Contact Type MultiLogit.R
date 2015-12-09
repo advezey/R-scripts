0bfe30c4c3cfe7b5ee2fd2e880bafa595bd9a3b4
advezey <andrew.vezey@staples.com>
2015-12-09 12:43:24 -0700

    With random sample code

--- FCR Issue Type_Order Issue Type Name to Contact Type MultiLogit.R
+++ FCR Issue Type_Order Issue Type Name to Contact Type MultiLogit.R
@@ -8,7 +8,7 @@ connTOAD <- odbcConnect('Sunsysh', uid ='CCFLAXV',  pwd = 'Gandalf8')
 
 fcr <- sqlQuery(connSQL, "
                   select a.*, b.OrderIssueID,b.OrderIssuePartVItem, b.OrderIssueFollowup, b.OrderIssueDate, b.CSARNetworkID
-                , e.OrderIssueTypeSort_SAUS as IssueType,c.OrderIssueContactID,c.RootCauseID, c.UserLANID as lanid
+		, e.OrderIssueTypeSort_SAUS as IssueType,c.OrderIssueContactID,c.RootCauseID, c.UserLANID as lanid
                 , c.ContactDate, d.*, e.OrderIssueTypeName , f.ContactTypeName
                 from FCR.dbo.FCR_Order a
                 inner join FCR.dbo.FCR_OrderIssue b on a.OrderID=b.OrderID 
@@ -17,21 +17,25 @@ fcr <- sqlQuery(connSQL, "
                 left join FCR.dbo.FCR_OrderIssueType e on b.OrderIssueTypeID=e.OrderIssueTypeID 
                 left join FCR.dbo.FCR_ContactType F on f.ContactTypeID=c.ContactTypeID
                 where b.CSARNetworkID = 2
-                AND c.ContactDate >= '2015-11-15'"
+                AND c.ContactDate >= '2015-10-10'"
                 , stringsAsFactors=F)
 
-trainingdata <- fcr[1:10000,c(9,18,19)]
+trainingdata <- fcr[sample(1:nrow(fcr),10000,replace = FALSE),c(9,18,19)]
 trainingdata$IssueType <- as.factor(trainingdata$IssueType)
 trainingdata$OrderIssueTypeName <- as.factor(trainingdata$OrderIssueTypeName)
 trainingdata$ContactTypeName <- as.factor(trainingdata$ContactTypeName)
 trainingdata$ContactTypeName2 <- relevel(trainingdata$ContactTypeName, ref = 'Phone')
 
-tdmodel <- multinom(ContactTypeName2 ~ IssueType + OrderIssueTypeName, data = trainingdata)
+tdmodel <- multinom(ContactTypeName2 ~ IssueType, data = trainingdata)
 z <- summary(tdmodel)$coefficients/summary(tdmodel)$standard.errors
 p <- (1 - pnorm(abs(z), 0, 1)) * 2
 
 summary(tdmodel)
 
-probtest <- data.frame(IssueType = c('Customer Request','Product','Tracking/Status')
-                       ,OrderIssueTypeName = ('Other'))
-predict(tdmodel, newdata=probtest,"probs")
+probtest <- data.frame(IssueType = unique(trainingdata$IssueType))
+probtest[,2:6]<-predict(tdmodel, newdata=probtest,"probs")
+        colnames(probtest) <- c('IssueType','Phone','Chat','Email'
+                                ,'Fax', 'Unknown')
+        probtest[,2:6] <- format(probtest[,2:6], scientific=FALSE)
+
+# tdsample <- fcr[sample(1:nrow(fcr),10000,replace = FALSE),c(2,5)]       
