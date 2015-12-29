51cb0553270f283ed79e396f2aa61fd2dd5b95ab
advezey <andrew.vezey@staples.com>
2015-12-29 13:57:47 -0700

    Example of ARIMA using SAUS contacts by period. Auto.arima not nearly as good of a tool as manual Arima() function.

---
+++ ARIMA example_Staples Advantage.R
@@ -0,0 +1,48 @@
+library(corrgram);library(doBy);library(stringr);library(reshape2);
+library(dplyr);library(ggplot2);library(RODBC);library(readxl);
+library(moments); library(aod); library(foreign);library(nnet);
+library(kSamples);library(forecast)
+
+connSQL <- odbcConnect('SQLServer')
+
+RawVolumeData <- sqlQuery(connSQL,paste0("select businessunit, fiscal_period_name_long, a.calendar_date, skillcategory, skillgroup, skillname
+	     , sum(anstime) as anstime, sum(abncalls) as abncalls, sum(acdcalls) as acdcalls
+                                         , sum(acdtime) as acdtime, sum(holdtime) as holdtime, sum(handledtime) as handletime
+                                         , sum(handledcalls) as handledcalls, sum(acwtime) as acwtime, sum(i_acwtime) as i_acwtime
+                                         from falcon.dbo.tblAvayaIntradaySkills a
+                                         join falcon.dbo.tlk_calendar b on a.calendar_date=b.calendar_date
+                                         where businessunit = 'Staples Advantage'
+                                         and a.calendar_date > getdate()-1123
+                                         group by businessunit, fiscal_period_name_long, a.calendar_date, skillcategory, skillgroup, skillname
+                                         order by a.calendar_date, skillcategory, skillgroup, skillname"))
+
+AggVolumebyPeriod <- aggregate.data.frame(RawVolumeData$acdcalls
+                                          ,list(RawVolumeData$fiscal_period_name_long)
+                                          ,sum)
+        AggVolumebyPeriod[1,2] <- 270000
+        AggVolumebyPeriod[nrow(AggVolumebyPeriod),2] <- 270000
+        colnames(AggVolumebyPeriod) <- c('Fiscal Period','ACDCalls')
+
+TimeSeries <- ts(AggVolumebyPeriod$ACDCalls)
+
+ARIMA <- auto.arima(TimeSeries, d=1)
+        acf(residuals(ARIMA))
+        Box.test(residuals(ARIMA),type = "Ljung-Box")
+        summary(ARIMA)
+        plot(residuals(ARIMA))
+
+Prediction <- forecast.Arima(ARIMA,level = 80)
+        plot(Prediction)
+        summary(Prediction)
+
+ARIMA2 <- Arima(TimeSeries,order = c(11,1,2),include.mean = FALSE
+                ,include.drift = TRUE)
+        acf(residuals(ARIMA2))
+        Box.test(residuals(ARIMA2),type = "Ljung-Box")
+        summary(ARIMA2)
+        plot(residuals(ARIMA2))
+
+Prediction2 <- forecast.Arima(ARIMA2, h = 12, level = 80)
+        plot(Prediction2)
+        summary(Prediction2)
+        
